5c71fc06ebc1005cbdb62ec416c474905bfd2081
Lawrence D'Anna <larry@elder-gods.org>
2015-07-22 19:38:15 -0700

    bear: woo GitPatchDownloadStrategy

--- bear.rb
+++ bear.rb
@@ -1,16 +1,27 @@
+
+class GitPatchDownloadStrategy < GitDownloadStrategy
+  def initialize name, resource
+    super
+    @baseref = meta[:base]
+  end
+  def stage
+    Homebrew.system("git", "-C", cached_location, "diff", @baseref, @ref) do
+      $stdout.reopen('patch')
+    end
+  end
+end
+
 class Bear < Formula
   desc "Generate compilation database for clang tooling"
   homepage "https://github.com/rizsotto/Bear"
 
-  # url "https://github.com/rizsotto/Bear/archive/2.0.3.tar.gz"
-  # sha256 "e9d217465198453ce87237e650dedb77f92cb530a10eac53b4a062ba779bd6c1"
+  url "https://github.com/rizsotto/Bear/archive/2.0.3.tar.gz"
+  sha256 "e9d217465198453ce87237e650dedb77f92cb530a10eac53b4a062ba779bd6c1"
 
   head "https://github.com/rizsotto/Bear.git"
-  
-  def patch
-    url = "https://github.com/smoofra/Bear.git"
-    ohai "merging patches from #{url}"
-    system "git", "pull", "--no-edit", url, "master"
+
+  patch :p1 do
+    url "https://github.com/smoofra/Bear.git", :using => GitPatchDownloadStrategy, :base => "2.0.4"
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
